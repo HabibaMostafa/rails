7665aef6285c3e3ff87a442eec3750d77a4babcc
Habiba Mostafa <mostafah@cis3260.socs.uoguelph.ca>
2021-10-25 03:07:46 -0400

    minor edits

--- CupThrow/app/controllers/home_controller.rb
+++ CupThrow/app/controllers/home_controller.rb
@@ -1,8 +1,13 @@
+# require '../../A1/A1_Classes.rb'
+
 class HomeController < ApplicationController
   def index
   end
 
   def display_user_info 
     # data comes through params 
+    current_user.points += 2
+    puts current_user.points
+
   end 
 end
